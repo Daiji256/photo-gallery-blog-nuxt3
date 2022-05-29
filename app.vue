aec6c80eec4a73d7097164c33dd31440a5bcab0d
Daiji256 <butty.main@gmail.com>
2022-05-29 18:43:41 +0900

    フォントの追加と修正

--- app.vue
+++ app.vue
@@ -10,5 +10,11 @@
 useHead({
 	htmlAttrs: [{ lang: 'ja' }],
 	meta: [{ charset: 'utf-8' }],
+	link: [
+		{
+			rel: 'stylesheet',
+			href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;500;700&display=swap',
+		},
+	],
 })
 </script>
