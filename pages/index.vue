c542ce3e0307731a276d11c392b173459f52c67e
Daiji256 <butty.main@gmail.com>
2022-05-31 12:59:26 +0900

    ダミーテキストを修正

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,26 @@
 <template>
 	<div>
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
-		abc ABC !@# あいう 漢字 ！＠＃😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
+		abcgi ABCGI 123 !@# あいう 漢字 ！＠＃ 😀🎈
 	</div>
 </template>
 
