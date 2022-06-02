454e753fcd2c62b7c3bcd2b7a353394356006f7b
Daiji256 <butty.main@gmail.com>
2022-06-02 10:04:12 +0900

    header をナビゲーションではないようにした

--- app.vue
+++ app.vue
@@ -9,7 +9,10 @@
 <script setup>
 useHead({
 	htmlAttrs: [{ lang: 'ja' }],
-	meta: [{ charset: 'utf-8' }],
+	meta: [
+		{ charset: 'utf-8' },
+		{ name: 'viewport', content: 'width=device-width, initial-scale=1.0' },
+	],
 	link: [
 		{
 			rel: 'preconnect',
--- components/HeaderNavigation.vue
+++ components/HeaderNavigation.vue
@@ -1,52 +1,20 @@
 <template>
 	<header>
-		<div class="container">
-			<ul>
-				<li>
-					<NuxtLink to="/">
-						<div class="icon">
-							<svg height="24" width="24">
-								<path fill="currentColor"
-									d="M4 21V9L12 3L20 9V21H14V14H10V21ZM6 19H8V12H16V19H18V10L12 5.5L6 10ZM12 12.25Z" />
-							</svg>
-						</div>
-						<div class="label">ホーム</div>
-					</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/tags">
-						<div class="icon">
-							<svg height="24" width="24">
-								<path fill="currentColor"
-									d="M6 9H18V4H6ZM4 11V2H20V11ZM6 20H18V15H6ZM4 22V13H20V22ZM6 9V4V9ZM6 20V15V20Z" />
-							</svg>
-						</div>
-						<div class="label">全ての投稿</div>
-					</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/tags">
-						<div class="icon">
-							<svg height="24" width="24">
-								<path fill="currentColor"
-									d="M6 20 7 16H3.5L4 14H7.5L8.5 10H4.5L5 8H9L10 4H12L11 8H15L16 4H18L17 8H20.5L20 10H16.5L15.5 14H19.5L19 16H15L14 20H12L13 16H9L8 20ZM9.5 14H13.5L14.5 10H10.5Z" />
-							</svg>
-						</div>
-						<div class="label">タグ</div>
-					</NuxtLink>
-				</li>
-				<li>
-					<NuxtLink to="/tags">
-						<div class="icon">
-							<svg height="24" width="24">
-								<path fill="currentColor"
-									d="M3 11V3H11V11ZM3 21V13H11V21ZM13 11V3H21V11ZM13 21V13H21V21ZM5 9H9V5H5ZM15 9H19V5H15ZM15 19H19V15H15ZM5 19H9V15H5ZM15 9ZM15 15ZM9 15ZM9 9Z" />
-							</svg>
-						</div>
-						<div class="label">ギャラリー</div>
-					</NuxtLink>
-				</li>
-			</ul>
+		<div class="menu">
+			<svg height="24" width="24">
+				<path fill="currentColor" d="M3 18V16H21V18ZM3 13V11H21V13ZM3 8V6H21V8Z" />
+			</svg>
+		</div>
+		<div class="title">
+			<div class="logo">
+				<svg height="24" width="24">
+					<path fill="currentColor"
+						d="M12 22Q9.925 22 8.1 21.212Q6.275 20.425 4.925 19.075Q3.575 17.725 2.788 15.9Q2 14.075 2 12Q2 9.925 2.788 8.1Q3.575 6.275 4.925 4.925Q6.275 3.575 8.1 2.787Q9.925 2 12 2Q14.075 2 15.9 2.787Q17.725 3.575 19.075 4.925Q20.425 6.275 21.212 8.1Q22 9.925 22 12Q22 14.075 21.212 15.9Q20.425 17.725 19.075 19.075Q17.725 20.425 15.9 21.212Q14.075 22 12 22ZM12 20Q15.35 20 17.675 17.675Q20 15.35 20 12Q20 8.65 17.675 6.325Q15.35 4 12 4Q8.65 4 6.325 6.325Q4 8.65 4 12Q4 15.35 6.325 17.675Q8.65 20 12 20ZM12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Q12 12 12 12Z" />
+				</svg>
+			</div>
+			<div class="text">
+				Photo Gallery Blog
+			</div>
 		</div>
 	</header>
 </template>
