d735d8464b3076f1f96516d44af97d9e7d56d7f4
Daiji256 <butty.main@gmail.com>
2022-06-18 09:25:40 +0900

    静的出力の設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,7 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+	target: 'static',
+	ssr: false,
 	css: ['modern-css-reset', '~/assets/scss/main.scss'],
 })
