485ba26104692728fe77acb9f6fbf9433bd443b7
Daiji256 <butty.main@gmail.com>
2022-06-19 18:03:38 +0900

    記事の一覧を表示

---
+++ content/pages/test7/index.md
@@ -0,0 +1,7 @@
+---
+title: Title 7
+date: 2022-01-05T00:00:00+09:00
+_draft: true
+---
+
+text
--- pages/all-posts.vue
+++ pages/all-posts.vue
@@ -1,57 +1,18 @@
 <template>
 	<div>
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
-		abcgi ABCGI 123 !@# あいう 漢字 あ「い」（う）え。お ！＠＃ 😀🎈
+		<div v-for="(page, index) in pages" :key="index">
+			<nuxt-link :to="page._path">{{ page.title }}</nuxt-link>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+const pages = await queryContent('pages')
+	.only(['_path', 'title', 'date'])
+	.where({ '_draft': false })
+	.sort({ 'date': 1 })
+	.find();
+
 definePageMeta({
 	layout: 'main-wrapper',
 });
