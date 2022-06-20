b5caffc2b30213d646d3f47825980eb2ccdb73d8
Daiji256 <butty.main@gmail.com>
2022-06-20 21:56:16 +0900

    記事リストの概要を作成

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,14 +1,19 @@
 <template>
-	<div>
-		<div v-for="(page, index) in pages" :key="index">
-			<nuxt-link :to="page._path">{{ page.title }}</nuxt-link>
+	<div class="post-cards">
+		<div class="post-card" v-for="(page, index) in pages" v-bind:key="index">
+			<nuxt-link v-bind:to="page._path">
+				<img src="/images/sample-20.jpg">
+				<div class="title">{{ page.title }}</div>
+				<div class="tags">{{ page.tags }}</div>
+				<div class="date">{{ page.date }}</div>
+			</nuxt-link>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
 const pages = await queryContent('pages')
-	.only(['_path', 'title', 'date'])
+	.only(['_path', 'title', 'date', 'tags'])
 	.where({ '_draft': false })
 	.sort({ 'date': 1 })
 	.find();
