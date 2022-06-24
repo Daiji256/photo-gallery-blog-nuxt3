685b0d03f3cf75b9db76e79eef37e3f3ba168264
Daiji256 <butty.main@gmail.com>
2022-06-24 21:37:51 +0900

    page -> post

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,18 +1,18 @@
 <template>
 	<div class="post-cards">
-		<div class="post-card" v-for="(page, index) in pages" v-bind:key="index">
-			<nuxt-link v-bind:to="page._path">
+		<div class="post-card" v-for="(post, index) in posts" v-bind:key="index">
+			<nuxt-link v-bind:to="post._path">
 				<img src="/images/sample-20.jpg">
-				<div class="title">{{ page.title }}</div>
-				<div class="tags">{{ page.tags }}</div>
-				<div class="date">{{ page.date }}</div>
+				<div class="title">{{ post.title }}</div>
+				<div class="tags">{{ post.tags }}</div>
+				<div class="date">{{ post.date }}</div>
 			</nuxt-link>
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
-const pages = await queryContent('pages')
+const posts = await queryContent('posts')
 	.only(['_path', 'title', 'date', 'tags'])
 	.where({ '_draft': false })
 	.sort({ 'date': 1 })
