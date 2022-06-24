a91b354ffc58408ebbd2b303ade405cd5423698b
Daiji256 <butty.main@gmail.com>
2022-06-25 08:03:10 +0900

    tag をちゃんと表示

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,10 +1,14 @@
 <template>
 	<div class="post-cards">
-		<div class="post-card" v-for="(post, index) in posts" v-bind:key="index">
+		<div class="post-card" v-for="post in posts">
 			<nuxt-link v-bind:to="post._path">
 				<img src="/images/sample-20.jpg">
 				<div class="title">{{ post.title }}</div>
-				<div class="tags">{{ post.tags }}</div>
+				<div class="tags">
+					<nuxt-link class="tag" v-for="tag in post.tags" v-bind:to="`/tags/${tag}/`">
+						{{ tag }}
+					</nuxt-link>
+				</div>
 				<div class="date">{{ post.date }}</div>
 			</nuxt-link>
 		</div>
