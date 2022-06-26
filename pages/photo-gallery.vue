a36cdada5b2946a3b95ab08de37d34a3c1abf02e
Daiji256 <butty.main@gmail.com>
2022-06-26 17:20:48 +0900

    ギャラリーを作成

--- pages/photo-gallery.vue
+++ pages/photo-gallery.vue
@@ -1,8 +1,17 @@
 <template>
-	<div>
-		フォトギャラリー
+	<div class="photo-gallery">
+		<div class="photo-card" v-for="post in posts">
+			<nuxt-link v-bind:to="post._path">
+				<img v-bind:src="`${post.image}`">
+			</nuxt-link>
+		</div>
 	</div>
 </template>
 
 <script setup lang="ts">
+const posts = await queryContent('posts')
+	.where({ '_draft': false })
+	.sort({ 'date': -1 })
+	.only(['_path', 'image'])
+	.find();
 </script>
