1a53e03da1067ec67f796abf7de65e9918d88aba
Daiji256 <butty.main@gmail.com>
2022-06-26 21:11:42 +0900

    タグの一覧を表示

--- pages/tags.vue
+++ pages/tags.vue
@@ -1,8 +1,28 @@
 <template>
-	<div>
-		タグ
+	<div class="tags">
+		<nuxt-link class="tag" v-for="tag in tags" v-bind:to="`/tags/${tag.name}/`">
+			{{ tag.name }} ({{ tag.count }})
+		</nuxt-link>
 	</div>
 </template>
 
 <script setup lang="ts">
+const postsTags = await queryContent('posts')
+	.where({ '_draft': false })
+	.only(['tags'])
+	.find();
+
+const tags = Object.entries(
+	[postsTags].flat().map(v => v.tags).flat().reduce(
+		(prev, current) => {
+			prev[current] = (prev[current] || 0) + 1;
+			return prev;
+		}, {}
+	)
+)
+	.map(([name, count]) => ({ name, count }))
+	.sort((a, b) => { return b.count - a.count; });
+
+
+const hoge = ref(tags);
 </script>
