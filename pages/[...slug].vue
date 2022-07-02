aac327cc32e4cbd401c3c128aefb515f46bde1d9
Daiji256 <butty.main@gmail.com>
2022-07-02 10:54:46 +0900

    markdown の処理をちょっと開いた

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,8 +1,18 @@
 <template>
 	<div>
-		<ContentDoc />
+		<div>{{ content.title }}</div>
+		<div>{{ content.date }}</div>
+		<div>{{ content.tags }}</div>
+		<img v-bind:src="content.image" width="250px">
+		<!-- TODO: ContentRendererMarkdown で警告 -->
+		<ContentRenderer v-bind:value="content">
+			<ContentRendererMarkdown v-bind:value="content" />
+		</ContentRenderer>
 	</div>
 </template>
 
 <script setup lang="ts">
+// TODO: queryContent の引数で警告
+const content = await queryContent(useRoute().path).findOne();
+// TODO: content 内の value でアキの処理をする
 </script>
