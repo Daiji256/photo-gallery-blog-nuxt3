bd90fd4d07c65736c635eeb2f1e52fefd30d3d5f
Daiji256 <butty.main@gmail.com>
2022-07-14 08:49:19 +0900

    投稿の絞り込み機能を追加

--- pages/posts.vue
+++ pages/posts.vue
@@ -1,16 +1,25 @@
 <template>
-	<div class="post-cards">
-		<div class="post-card" v-for="post in posts">
-			<nuxt-link v-bind:to="post._path">
-				<img v-bind:src="`${post.image}`">
-				<div class="title">{{ post.title }}</div>
-				<div class="tags">
-					<nuxt-link class="tag" v-for="tag in post.tags" v-bind:to="`/tags/${tag}/`">
-						{{ tag }}
-					</nuxt-link>
+	<div>
+		<div class="tags">
+			<div class="tag" v-for="tag in tags" v-on:click="onClickTag(tag.name)">
+				{{ tag.name }} ({{ tag.count }})
+			</div>
+		</div>
+		<div class="post-cards">
+			<div v-for="post in posts">
+				<div class="post-card" v-if="containsTag(post.tags)">
+					<NuxtLink v-bind:to="post._path">
+						<img v-bind:src="`${post.image}`">
+						<div class="title">{{ post.title }}</div>
+						<div class="tags">
+							<NuxtLink class="tag" v-for="tag in post.tags" v-bind:to="`/posts?tag=${tag}`">
+								{{ tag }}
+							</NuxtLink>
+						</div>
+						<div class="date">{{ dateJa(post.date) }}</div>
+					</NuxtLink>
 				</div>
-				<div class="date">{{ dateJa(post.date) }}</div>
-			</nuxt-link>
+			</div>
 		</div>
 	</div>
 </template>
@@ -21,6 +30,33 @@ const posts = await queryContent('posts')
 	.sort({ 'date': -1 })
 	.only(['_path', 'title', 'date', 'tags', 'image'])
 	.find();
+
+const tags = Object.entries(
+	[posts].flat().map(v => v.tags).flat().reduce(
+		(prev, current) => {
+			prev[current] = (prev[current] || 0) + 1;
+			return prev;
+		}, {}
+	)
+)
+	.map(([name, count]) => ({ name, count }))
+	.sort((a, b) => { return b.count - a.count; });
+
+const queryTags = [useRoute().query.tag].flat().filter(tag => tag != null);
+
+const onClickTag = (tagName) => {
+	if (queryTags.includes(tagName)) {
+		queryTags.splice(queryTags.indexOf(tagName), 1);
+	} else {
+		queryTags.push(tagName);
+	}
+	useRouter().push({ query: { tag: queryTags } });
+}
+
+const containsTag = (postTags) => {
+	if (queryTags.length === 0) return true;
+	return queryTags.some(tag => postTags.includes(tag));
+}
 
 const dateJa = (date: string) => {
 	return date
