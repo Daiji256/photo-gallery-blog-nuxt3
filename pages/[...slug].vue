1e0a61d7e57b1e0477d5ba917831be76b6e4c973
Daiji256 <butty.main@gmail.com>
2022-07-29 22:11:39 +0900

    それっぽいタイトル等にした

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,14 +1,25 @@
 <template>
 	<div class="post">
 		<ContentDoc v-slot="{ doc }">
-			<div>{{ doc.title }}</div>
-			<div>{{ doc.date }}</div>
-			<div>{{ doc.tags }}</div>
-			<img v-bind:src="doc.image" width=250>
+			<img v-bind:src="doc.image" class="top-image">
+			<h1 class="top-title">{{ doc.title }}</h1>
+			<div class="top-tag" v-for="tag in doc.tags">
+				{{ tag }}
+			</div>
+			<div class="top-date">{{ dateJa(doc.date) }}</div>
+
 			<ContentRenderer v-bind:value="doc" />
 		</ContentDoc>
 	</div>
 </template>
 
 <script setup lang="ts">
+const dateJa = (date: string) => {
+	return date
+		.substring(0, date.indexOf('T'))
+		.replaceAll('-0', '-')
+		.replace('-', '年')
+		.replace('-', '月')
+		.concat('日');
+}
 </script>
