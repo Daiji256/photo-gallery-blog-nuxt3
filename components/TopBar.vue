0c77164bce7c8d53dc3143039d51942d229a25db
Daiji256 <butty.main@gmail.com>
2022-07-30 19:53:48 +0900

    ヘッダーのボタンでドロワーを閉じるようにした

--- components/TopBar.vue
+++ components/TopBar.vue
@@ -9,7 +9,7 @@
 					d="M3 18V16H16V18ZM3 13V11H13V13ZM3 8V6H16V8ZM19.6 17 14.6 12 19.6 7 21 8.4 17.4 12 21 15.6Z" />
 			</svg>
 		</div>
-		<NuxtLink class="title-box" to="/">
+		<NuxtLink class="title-box" to="/" v-on:click="closeMenu">
 			<div class="title">
 				<div class="logo">
 					<svg height="24" width="24">
