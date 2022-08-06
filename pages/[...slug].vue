103b8c79b8133c47ae3fd783f9e545e56e82100b
Daiji256 <butty.main@gmail.com>
2022-08-06 15:01:58 +0900

    class="post" を外に出す

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,16 +1,18 @@
 <template>
-	<ContentDoc class="post" v-slot="{ doc }">
-		<img v-bind:src="doc.image" class="top-image">
-		<div class="post">
-			<h1 class="top-title">{{ doc.title }}</h1>
-			<NuxtLink class="top-tag" v-for="tag in doc.tags" v-bind:to="`/posts?tag=${tag}`">
-				{{ tag }}
-			</NuxtLink>
-			<div class="top-date">{{ dateJa(doc.date) }}</div>
+	<div class="post">
+		<ContentDoc v-slot="{ doc }">
+			<img v-bind:src="doc.image" class="top-image">
+			<div class="post">
+				<h1 class="top-title">{{ doc.title }}</h1>
+				<NuxtLink class="top-tag" v-for="tag in doc.tags" v-bind:to="`/posts?tag=${tag}`">
+					{{ tag }}
+				</NuxtLink>
+				<div class="top-date">{{ dateJa(doc.date) }}</div>
 
-			<ContentRenderer v-bind:value="doc" />
-		</div>
-	</ContentDoc>
+				<ContentRenderer v-bind:value="doc" />
+			</div>
+		</ContentDoc>
+	</div>
 </template>
 
 <script setup lang="ts">
