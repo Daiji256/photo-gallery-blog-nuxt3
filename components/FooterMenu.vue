ba7d30df3a1c6cf34aa1b8e5b75fdc18be26d39a
Daiji <butty.main@gmail.com>
2022-08-11 21:11:01 +0900

    footer の scss を移動，デザイン変更

--- components/FooterMenu.vue
+++ components/FooterMenu.vue
@@ -1,3 +1,16 @@
 <template>
 	<footer>&copy; 2022 Daiji256</footer>
 </template>
+
+<style lang="scss" scoped>
+@import '../assets/scss/variable';
+
+footer {
+  @extend .font-label-medium;
+  display: flex;
+  bottom: 0;
+  height: 96px;
+  justify-content: center;
+  align-items: center;
+}
+</style>
