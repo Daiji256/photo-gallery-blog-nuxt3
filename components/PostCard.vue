331a7a2cc86c70fb72ba52187dc817ad33bf8a78
Daiji <butty.main@gmail.com>
2022-08-11 22:06:27 +0900

    hover の設定

--- components/PostCard.vue
+++ components/PostCard.vue
@@ -1,7 +1,9 @@
 <template>
 	<div class="post-card">
 		<NuxtLink v-bind:to="path">
-			<img class="post-image" v-bind:src="image">
+			<div class="post-image-background">
+				<img class="post-image" v-bind:src="image">
+			</div>
 			<div class="post-info">
 				<div class="post-title">{{ title }}</div>
 				<TagsAndDate v-bind:date="date" v-bind:tags="tags" />
@@ -21,16 +23,44 @@
 	border-radius: 12px;
 	background-color: $color-surface;
 
+	@media (hover: hover) {
+		&:hover {
+			background-color: rgba($color-on-surface-variant, $state-hover);
+		}
+
+		.post-image-background {
+			background-color: $color-on-surface-variant;
+
+			.post-image:hover {
+				opacity: calc(100% - $state-hover);
+			}
+		}
+	}
+
+	&:active {
+		background-color: rgba($color-on-surface-variant, $state-pressed);
+
+		.post-image-background {
+			background-color: $color-on-surface-variant;
+
+			.post-image:active {
+				opacity: calc(100% - $state-pressed);
+			}
+		}
+	}
+
 	a {
 		text-decoration: none;
 		color: inherit;
 		-webkit-tap-highlight-color: transparent;
 	}
 
-	.post-image {
-		width: 100%;
-		aspect-ratio: $golden-ratio;
-		object-fit: cover;
+	.post-image-background {
+		.post-image {
+			width: 100%;
+			aspect-ratio: $golden-ratio;
+			object-fit: cover;
+		}
 	}
 
 	.post-info {
