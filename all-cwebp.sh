3e39b20612fd2435c7e4a4731a484493254ea063
Daiji <butty.main@gmail.com>
2022-08-20 23:50:56 +0900

    セミコロンをつけた

--- all-cwebp.sh
+++ all-cwebp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-images=`find public/images -type f -name \*.jpg -or -name \*.png`
+images=`find public/images -type f -name \*.jpg -or -name \*.png`;
 for image in $images;
-  do cwebp -q 80 $image -o "${image%.*}.webp"
+  do cwebp -q 80 $image -o "${image%.*}.webp";
 done
