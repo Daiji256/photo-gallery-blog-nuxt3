df0c8428e54e25a1efebebce667131a78db9980c
Daiji <butty.main@gmail.com>
2022-08-21 11:20:37 +0900

    画像をより圧縮

--- convert_webp.sh
+++ convert_webp.sh
@@ -3,5 +3,5 @@
 
 images=`find public/images -type f -name \*.jpg -or -name \*.png`;
 for image in $images;
-  do convert $image -resize 1024x1024 -quality 75 "${image%.*}.webp";
+  do convert $image -resize 640x640 -quality 75 -define webp:method=6 "${image%.*}.webp";
 done
