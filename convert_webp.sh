220c6dac075bbb36ff54327f73ed74350804e644
Daiji <butty.main@gmail.com>
2022-08-21 15:09:23 +0900

    ogp を作成，修正

--- convert_webp.sh
+++ convert_webp.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 # `imagemagick` is required
 
-images=`find public/images -type f -name \*.jpg -or -name \*.png`;
-for image in $images;
-  do convert $image -resize 640x640 -quality 75 -define webp:method=6 "${image%.*}.webp";
+images=`find public/images -type f -name \*.jpg -or -name \*.png`
+for image in $images
+do
+  convert $image -resize 640x396^ -quality 75 -define webp:method=6 "${image%.*}.webp"
+  convert $image -resize 1200x630^ -gravity center -crop 1200x630+0+0 -quality 75 "${image%.*}-ogp.jpg"
 done
